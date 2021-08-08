5bc2371a49ac6378fcc1efd46e778a0b8e784f27
lebalz <lebalz@outlook.com>
2021-08-08 21:55:46 +0000

    Deploy website - based on 1130ae93e0e080177026c74a9d56ad3c1bb9534d

--- assets/js/a06eef2e.1b26805d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkofi_blog_versioned=self.webpackChunkofi_blog_versioned||[]).push([[3687],{8639:function(e){"use strict";e.exports=JSON.parse('{"pluginId":"default","version":"24f","label":"24f","banner":"none","isLast":false,"docsSidebars":{"version-24f/sidebar":[{"type":"link","label":"24f","href":"/ofi-blog/24f/home"}]}}')}}]);
